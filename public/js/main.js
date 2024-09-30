383b33a961c914ff576c6b766dac5b5a2ca38866
altaparro <matiasaltaparro@hotmail.com>
2024-09-30 12:08:11 -0300

    fix main

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,31 @@
-const socket = io();
-
-socket.on("products", (data) => {
-    renderProducts(data);
-})
-
 const renderProducts = (data) => {
     const containerProducts = document.getElementById("containerProducts");
-    containerProducts.innerHTML = "";
-
-    data.forEach(item => {
-        const card = document.createElement("div");
-        card.classList.add("product-card");
-
-        card.innerHTML = `  
-            <h4 class="product-id">ID: ${item._id}</h4>
-            <h4 class="product-id">Código: ${item.code}</h4>
-            <h1 class="product-title">${item.title}</h1>
-            <h2 class="product-description">${item.description}</h2>
-            <h2 class="product-stock">Stock disponible: ${item.stock} | Categoría: ${item.category}</h2>
-            <p class="product-price">Precio: $${item.price}</p>
-            <button class="trash">ELIMINAR</button>
-        `;
-        containerProducts.appendChild(card);
-        card.querySelector("button").addEventListener("click", () => {
-            removeProduct(item._id);
-        });
-    });
-};
-
-const removeProduct = (id) => {
-    socket.emit("removeProduct", id);
+    containerProducts.innerHTML = ""; // Limpiar el contenedor de productos antes de renderizar nuevos productos
 };
 
 document.getElementById("btnEnviar").addEventListener("click", () => {
-    addProduct();
-    cleanForm();
+    addProduct(); // Llamar a la función para agregar el producto
+    cleanForm(); // Limpiar el formulario después de agregar el producto
 });
-
-const addProduct = () => {
-    const producto = {
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value,
-        price: document.getElementById("price").value,
-        img: document.getElementById("img").value,
-        code: document.getElementById("code").value,
-        stock: document.getElementById("stock").value,
-        category: document.getElementById("category").value,
-        status: document.getElementById("status").value === "true",
-    };
-
-    socket.emit("addProduct", producto);
-};
 
 function cleanForm() {
     const form = document.getElementById("formAddProduct");
-    form.reset();
+    form.reset(); // Reiniciar el formulario
 }
+
+document.getElementById('sortPrice').addEventListener('change', function () {
+    const sortOrder = this.value; // Obtener el orden de clasificación seleccionado
+    const currentUrl = new URL(window.location.href);
+    const page = currentUrl.searchParams.get('page') || 1; // Obtener la página actual
+    currentUrl.searchParams.set('sort', sortOrder); // Establecer el nuevo parámetro de orden
+    currentUrl.searchParams.set('page', page); // Mantener la misma página
+    window.location.href = currentUrl.toString(); // Redirigir a la URL actualizada
+});
+
+window.addEventListener('load', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const sortParam = urlParams.get('sort');
+    if (sortParam) {
+        document.getElementById('sortPrice').value = sortParam; // Establecer el valor de orden de clasificación al cargar la página
+    }
+});
